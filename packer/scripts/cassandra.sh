fa54167fbad3efa134c36eb39f598bb4c4e09461
Alejandro Galue <agalue@opennms.org>
2018-02-23 16:02:27 -0500

    Using latest Java with Cassandra.
    The problem with 8u161 I saw with 3.11.1 was solved on 3.11.2

--- packer/scripts/cassandra.sh
+++ packer/scripts/cassandra.sh
@@ -7,13 +7,6 @@ repo_version="311x"
 
 ########################################
 
-echo "### Downloading and installing Oracle JDK..."
-
-sudo yum install -y -q http://yum.opennms.org/repofiles/opennms-repo-stable-rhel7.noarch.rpm
-sudo rpm --import /etc/yum.repos.d/opennms-repo-stable-rhel7.gpg
-sudo yum install -y -q jdk1.8.0_144
-sudo yum erase -y -q opennms-repo-stable
-
 echo "### Downloading and installing Cassandra..."
 
 tmp_repo=/tmp/cassandra.repo 
