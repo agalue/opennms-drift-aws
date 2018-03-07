86acd35afc5b1b450110b0c00f2aae9f5e89280a
Alejandro Galue <agalue@opennms.org>
2018-03-07 12:20:38 -0500

    Minor fix on build.sh

--- packer/build.sh
+++ packer/build.sh
@@ -12,8 +12,8 @@ if hash packer 2>/dev/null; then
   packer build cassandra.json      -var "region=$region" -var "source_ami=$source_ami" && \
   packer build elasticsearch.json  -var "region=$region" -var "source_ami=$source_ami" && \
   packer build kafka.json          -var "region=$region" -var "source_ami=$source_ami" && \
-  packer build kibana.json         -var "region=$region" -var "source_ami=$source_ami"  && \
-  packer build opennms.json        -var "region=$region" -var "source_ami=$source_ami"  && \
+  packer build kibana.json         -var "region=$region" -var "source_ami=$source_ami" && \
+  packer build opennms.json        -var "region=$region" -var "source_ami=$source_ami" && \
   packer build postgresql.json     -var "region=$region" -var "source_ami=$source_ami" && \
   packer build zookeeper.json      -var "region=$region" -var "source_ami=$source_ami" 
 else
