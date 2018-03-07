1aa2c09aade94d0dcb188ea051bb632dfb994976
Alejandro Galue <agalue@opennms.org>
2018-03-07 12:23:07 -0500

    Fixing build.sh

--- packer/build.sh
+++ packer/build.sh
@@ -8,14 +8,14 @@ echo "AWS Source AMI: $source_ami"
 echo
 
 if hash packer 2>/dev/null; then
-  packer build activemq.json       -var "region=$region" -var "source_ami=$source_ami" && \
-  packer build cassandra.json      -var "region=$region" -var "source_ami=$source_ami" && \
-  packer build elasticsearch.json  -var "region=$region" -var "source_ami=$source_ami" && \
-  packer build kafka.json          -var "region=$region" -var "source_ami=$source_ami" && \
-  packer build kibana.json         -var "region=$region" -var "source_ami=$source_ami" && \
-  packer build opennms.json        -var "region=$region" -var "source_ami=$source_ami" && \
-  packer build postgresql.json     -var "region=$region" -var "source_ami=$source_ami" && \
-  packer build zookeeper.json      -var "region=$region" -var "source_ami=$source_ami" 
+  packer build -var "region=$region" -var "source_ami=$source_ami" activemq.json && \
+  packer build -var "region=$region" -var "source_ami=$source_ami" cassandra.json  && \
+  packer build -var "region=$region" -var "source_ami=$source_ami" elasticsearch.json && \
+  packer build -var "region=$region" -var "source_ami=$source_ami" kafka.json  && \
+  packer build -var "region=$region" -var "source_ami=$source_ami" kibana.json && \
+  packer build -var "region=$region" -var "source_ami=$source_ami" opennms.json  && \
+  packer build -var "region=$region" -var "source_ami=$source_ami" postgresql.json && \
+  packer build -var "region=$region" -var "source_ami=$source_ami" zookeeper.json 
 else
   echo "ERROR: Packer is not installed."
   echo "       Please go to https://packer.io/ and follow the instructions."
