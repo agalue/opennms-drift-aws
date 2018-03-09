ac40e1861790ebf33d2a940e20360e767a0842e2
Alejandro Galue <agalue@opennms.org>
2018-03-09 17:22:09 -0500

    Fixing common.sh to be sudo friendly

--- packer/scripts/common.sh
+++ packer/scripts/common.sh
@@ -8,6 +8,8 @@ max_files="100000"
 
 ########################################
 
+tmp_file=/tmp/_temp.tmp
+
 echo "### Configuring Timezone..."
 
 sudo ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
@@ -19,10 +21,9 @@ sudo yum -y -q install jq net-snmp net-snmp-utils git pytz dstat htop sysstat nm
 
 echo "### Configuring and enabling SNMP..."
 
-snmp_tmp=/tmp/snmpd.conf
 snmp_cfg=/etc/snmp/snmpd.conf
 sudo cp $snmp_cfg $snmp_cfg.original
-cat <<EOF > $snmp_tmp
+cat <<EOF > $tmp_file
 com2sec localUser default public
 group localGroup v1 localUser
 group localGroup v2c localUser
@@ -33,14 +34,15 @@ syscontact Account Manager
 dontLogTCPWrappersConnects yes
 disk /
 EOF
-sudo cp $snmp_tmp $snmp_cfg
+sudo mv $tmp_file $snmp_cfg
 sudo chmod 600 $snmp_cfg
 
 echo "### Configuring Kernel..."
 
-sed -i 's/^\(.*swap\)/#\1/' /etc/fstab
+sudo sed -i 's/^\(.*swap\)/#\1/' /etc/fstab
 
-cat <<EOF > /etc/sysctl.d/application.conf
+sysctl_cfg=/etc/sysctl.d/application.conf
+cat <<EOF > $tmp_file
 net.ipv4.tcp_keepalive_time=60
 net.ipv4.tcp_keepalive_probes=3
 net.ipv4.tcp_keepalive_intvl=10
@@ -58,8 +60,11 @@ net.core.somaxconn=65000
 
 vm.swappiness=1
 EOF
+sudo mv $tmp_file $sysctl_cfg
 
-cat <<EOF > /etc/security/limits.d/application.conf
+limits_cfg=/etc/security/limits.d/application.conf
+cat <<EOF > $tmp_file
 * soft nofile $max_files
 * hard nofile $max_files
 EOF
+sudo mv $tmp_file $limits_cfg
