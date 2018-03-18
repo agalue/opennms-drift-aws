d371f7269a5495a6d423bc1d0ae767ce4413419a
Alejandro Galue <agalue@opennms.org>
2018-03-18 14:37:48 -0500

    Bump Elasticsearch version to 6.2.2
    The logic to install the drift plugin has been updated.

--- packer/scripts/elasticsearch.sh
+++ packer/scripts/elasticsearch.sh
@@ -6,7 +6,8 @@
 
 ######### CUSTOMIZED VARIABLES #########
 
-es_version="6.1.1"
+es_version="6.2.2"
+curator_version="5.4.1"
 maven_version="3.5.3"
 plugin_branch="master"
 plugin_name="elasticsearch-drift-plugin-1.0.0-SNAPSHOT"
@@ -21,7 +22,7 @@ sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install --batch x-pack
 
 echo "### Downloading and installing Curator..."
 
-yum install -y -q https://packages.elastic.co/curator/5/centos/7/Packages/elasticsearch-curator-5.4.1-1.x86_64.rpm
+sudo yum install -y -q https://packages.elastic.co/curator/5/centos/7/Packages/elasticsearch-curator-${curator_version}-1.x86_64.rpm
 
 echo "### Installing Maven..."
 
@@ -40,13 +41,24 @@ cd
 
 echo "### Installing the OpenNMS Drift Plugin from Source..."
 
+sudo yum install -y -q rpm-build
+
 mkdir ~/development
 cd ~/development
 sudo git clone https://github.com/OpenNMS/elasticsearch-drift-plugin.git
+
 cd elasticsearch-drift-plugin
-sudo git checkout -b $plugin_branch origin/$plugin_branch
-sudo /opt/maven/bin/mvn install -q -DskipTests=true
-sudo unzip target/releases/$plugin_name.zip -d elasticsearch
-sudo zip /tmp/$plugin_name.zip elasticsearch/*
-sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install file:///tmp/$plugin_name.zip
+
+if [[ `git branch | grep "^[*] $plugin_branch" | sed -e 's/[* ]*//'` == "$plugin_branch" ]]; then
+  echo "### Already in branch $plugin_branch"
+else
+  echo "### Checking out branch $plugin_branch"
+  sudo git checkout -b $plugin_branch origin/$plugin_branch
+fi
+
+sudo /opt/maven/bin/mvn install -q -DskipTests=true rpm:rpm
+
+rpmfile=`find target -name *.rpm`
+sudo yum install -y -q $rpmfile
+
 cd
