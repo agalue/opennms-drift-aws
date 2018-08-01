8fe770347512f585ebdd2f6cd8a8c602c69d35d2
Alejandro Galue <agalue@opennms.org>
2018-08-01 14:42:32 -0400

    Adding a working but very old kafka-manager instance.

--- resources/kafka-manager/docker-start.sh
+++ resources/kafka-manager/docker-start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -p 9000:9000 -e ZK_HOSTS=zookeeper1:2181,zookeeper2:2181,zookeeper3:2181/kafka agalue/kafka-manager
+docker run --rm -p 9000:9000 -e ZK_HOSTS=zookeeper1:2181,zookeeper2:2181,zookeeper3:2181 sheepkiller/kafka-manager:alpine
