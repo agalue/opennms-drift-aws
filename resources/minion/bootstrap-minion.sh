3b31ddcf8b72130a4b6a192bf4bf6a24a1d49ee4
Alejandro Galue <agalue@opennms.org>
2019-01-16 14:20:30 -0500

    The parser, not the listener has to match the name of the adapter.

--- resources/minion/bootstrap-minion.sh
+++ resources/minion/bootstrap-minion.sh
@@ -281,53 +281,53 @@ syslog.queue.size=100000
 EOF
 
   cat <<EOF > org.opennms.features.telemetry.listeners-udp-50001.cfg
-name=NXOS
+name=NXOS-Listener
 class-name=org.opennms.netmgt.telemetry.listeners.UdpListener
 parameters.host=0.0.0.0
 parameters.port=50001
 parameters.maxPacketSize=16192
-parsers.0.name=NXOS-Parser
+parsers.0.name=NXOS
 parsers.0.class-name=org.opennms.netmgt.telemetry.protocols.common.parser.ForwardParser
 EOF
 
   cat <<EOF > org.opennms.features.telemetry.listeners-udp-8877.cfg
-name=Netflow-5
+name=Netflow-5-Listener
 class-name=org.opennms.netmgt.telemetry.listeners.UdpListener
 parameters.host=0.0.0.0
 parameters.port=8877
 parameters.maxPacketSize=8096
-parsers.0.name=Netflow-5-Parser
+parsers.0.name=Netflow-5
 parsers.0.class-name=org.opennms.netmgt.telemetry.protocols.netflow.parser.Netflow5UdpParser
 EOF
 
   cat <<EOF > org.opennms.features.telemetry.listeners-udp-4729.cfg
-name=Netflow-9
+name=Netflow-9-Listener
 class-name=org.opennms.netmgt.telemetry.listeners.UdpListener
 parameters.host=0.0.0.0
 parameters.port=4729
 parameters.maxPacketSize=8096
-parsers.0.name=Netflow-9-Parser
+parsers.0.name=Netflow-9
 parsers.0.class-name=org.opennms.netmgt.telemetry.protocols.netflow.parser.Netflow9UdpParser
 parsers.0.parameters.templateTimeout=1800000
 EOF
 
   cat <<EOF > org.opennms.features.telemetry.listeners-udp-6343.cfg
-name=SFlow
+name=SFlow-Listener
 class-name=org.opennms.netmgt.telemetry.listeners.UdpListener
 parameters.host=0.0.0.0
 parameters.port=6343
 parameters.maxPacketSize=8096
-parsers.0.name=SFlow-Parser
+parsers.0.name=SFlow
 parsers.0.class-name=org.opennms.netmgt.telemetry.protocols.sflow.parser.SFlowUdpParser
 EOF
 
   cat <<EOF > org.opennms.features.telemetry.listeners-udp-4738.cfg
-name=IPFIX
+name=IPFIX-Listener
 class-name=org.opennms.netmgt.telemetry.listeners.UdpListener
 parameters.host=0.0.0.0
 parameters.port=4738
 parameters.maxPacketSize=8096
-parsers.0.name=IPFIX-Parser
+parsers.0.name=IPFIX
 parsers.0.class-name=org.opennms.netmgt.telemetry.protocols.netflow.parser.IpfixUdpParser
 parsers.0.parameters.templateTimeout=1800000
 EOF
